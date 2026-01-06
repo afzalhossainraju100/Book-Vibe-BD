6f3d2faa6ae115d84ee6f4c4c1c532021779f89f
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2026-01-06 08:49:01 +0600

    banner section done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import bannerImage from "../../assets/books.jpg";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>hy i am banner</h1>
-        </div>
-    );
+  return (
+    <div className="banner flex justify-between items-center bg-[#F3F3F3] rounded-lg p-16 mx-4 mt-8 w-[90%] mx-auto mb-8">
+      <div className="flex flex-col gap-10">
+        <h1 className="text-[3.5rem]">
+          Books to freshen <br /> up your bookshelf
+        </h1>
+        <button className="btn bg-[#23BE0A] border-0 py-[21px] px-[28px] text-white hover:bg-[#1b8c08] w-[180px]">
+          View The List
+        </button>
+      </div>
+      <div>
+        <img src={bannerImage} alt="Bookshelf" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
